ea61753dfed0f264db3b12087f616fcc892ec0f2
Yury Golub <yuragolub21@gmail.com>
2023-06-27 11:51:00 +0300

    Update install-nuget.cmd.

--- install-nuget.cmd
+++ install-nuget.cmd
@@ -1,6 +1,13 @@
-@if not exist "C:\Tools\Nuget\" mkdir "C:\Tools\Nuget\"
+@ECHO OFF
 
-@if not exist "C:\Tools\Nuget\nuget.exe" (
-	cd /d "C:\Tools\Nuget\"
-	curl -OL https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-)
+if not exist "C:\Tools\Nuget\" mkdir "C:\Tools\Nuget\"
+
+if exist "C:\Tools\Nuget\nuget.exe" goto exit
+set currentDirTemp=%cd%
+cd /d "C:\Tools\Nuget\"
+curl -OL https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+echo Nuget installed to '%cd%'
+
+cd /d "%currentDirTemp%"
+
+:exit
