3af4c62dfa425850901db537d10e5186276d7ad8
Yury Golub <yuragolub21@gmail.com>
2023-08-11 16:26:16 +0300

    Update yt-dlp version. Small fixes.

--- download-songs.cmd
+++ download-songs.cmd
@@ -1,9 +1,9 @@
 @echo off
 
-where /Q yt-dlp
-if errorlevel 1	(
-    curl -OL https://github.com/yt-dlp/yt-dlp/releases/download/2023.03.04/yt-dlp.exe
-)
+set ytdlpUrl=https://github.com/yt-dlp/yt-dlp/releases/download/2023.07.06/yt-dlp.exe
+
+where /q yt-dlp
+if %ERRORLEVEL% NEQ 0 curl -OL "%ytdlpUrl%"
 
 set /p urls="Input url(s): "
 set /p output="Output path(empty for current folder: "%cd%"): "
