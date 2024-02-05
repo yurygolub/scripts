52b888f5404d79627cceaebab367c14ad0ce2b31
Yury Golub <yuragolub21@gmail.com>
2024-02-05 11:15:24 +0300

    Update script.

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -3,3 +3,5 @@ Set-Service DiagTrack -StartupType Disabled
 Disable-ScheduledTask -TaskPath 'microsoft\windows\application experience' -TaskName 'Microsoft Compatibility Appraiser'
 Disable-ScheduledTask -TaskPath 'microsoft\windows\application experience' -TaskName 'ProgramDataUpdater'
 Disable-ScheduledTask -TaskPath 'microsoft\windows\application experience' -TaskName 'StartupAppTask'
+
+[System.Environment]::SetEnvironmentVariable('DOTNET_CLI_TELEMETRY_OPTOUT', '1', [System.EnvironmentVariableTarget]::Machine)
