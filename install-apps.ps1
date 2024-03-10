7c8b6319642b445c3e6927a7de6ff985cdcec5ea
Yury Golub <yuragolub21@gmail.com>
2024-03-10 14:46:12 +0300

    Add install-apps.ps1 script.

---
+++ install-apps.ps1
@@ -0,0 +1,208 @@
+function Get-RedirectedUrl
+{
+    param (
+        [Parameter(Mandatory = $true)]
+        [uri]$url,
+        [uri]$referer
+    )
+
+    $request = [Net.WebRequest]::CreateDefault($url)
+    if ($referer)
+    {
+        $request.Referer = $referer
+    }
+
+    $response = $request.GetResponse()
+
+    if ($response -and $response.ResponseUri.OriginalString -ne $url)
+    {
+        Write-Verbose "Found redirected url '$($response.ResponseUri)'"
+        $result = $response.ResponseUri.OriginalString
+    }
+    else
+    {
+        Write-Warning 'No redirected url was found, returning given url.'
+        $result = $url
+    }
+
+    $response.Dispose()
+
+    return $result
+}
+
+function Install-Notepad
+{
+    $npp = Get-ChildItem 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall' |
+        Where-Object { $_.Name.Contains('Notepad++') }
+
+    if ($npp)
+    {
+        Write-Host "'$($npp.GetValue('DisplayName')) - $($npp.GetValue('DisplayVersion'))' already installed"
+        return
+    }
+
+    $baseUrl = 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases'
+    $redirected = Get-RedirectedUrl "$baseUrl/latest"
+
+    $tagName = Split-Path $redirected -Leaf
+    $nppInstaller = "npp.$($tagName.Substring(1)).Installer.exe"
+
+    $tempDir = 'temp'
+    $null = New-Item -Path $tempDir -Type Directory -Force
+
+    $nppInstallerPath = Join-Path $tempDir -ChildPath $nppInstaller
+    if (!(Test-Path -Path $nppInstallerPath))
+    {
+        $downloadUrl = "$baseUrl/download/$tagName/$nppInstaller"
+        Write-Host "Downloading '$nppInstaller' from '$downloadUrl'"
+        curl.exe -L $downloadUrl -o $nppInstallerPath
+    }
+
+    & $nppInstallerPath
+
+    Write-Host
+}
+
+function Install-Winget
+{
+    if (Get-Command -ErrorAction Ignore -Type Application winget.exe)
+    {
+        Write-Host 'winget already installed'
+        return
+    }
+
+    $isWingetExist = $true
+    $wingetName = 'Microsoft.DesktopAppInstaller'
+    $currentShell = [System.AppDomain]::CurrentDomain.FriendlyName
+    if ($currentShell -eq 'pwsh')
+    {
+        $command = "if (!(Get-AppxPackage $wingetName)) { exit 1 }"
+        $process = Start-Process PowerShell -NoNewWindow -PassThru -Wait -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"$command`""
+        if ($process.ExitCode -ne 0)
+        {
+            $isWingetExist = $false
+        }
+    }
+    else
+    {
+        if (!(Get-AppxPackage $wingetName))
+        {
+            $isWingetExist = $false
+        }
+    }
+
+    if ($isWingetExist)
+    {
+        Write-Host "'$wingetName' already exists"
+        return
+    }
+
+    $tempDir = 'temp'
+    $null = New-Item -Path $tempDir -Type Directory -Force
+
+    $wingetBundle = 'Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+    $wingetBundlePath = Join-Path $tempDir -ChildPath $wingetBundle
+
+    if (!(Test-Path -Path $wingetBundlePath))
+    {
+        $downloadUrl = "https://github.com/microsoft/winget-cli/releases/latest/download/$wingetBundle"
+        Write-Host "Downloading '$wingetBundle' from '$downloadUrl'"
+        curl.exe -L $downloadUrl -o $wingetBundlePath
+    }
+
+    Write-Host "Installing '$wingetName'"
+    if ($currentShell -eq 'pwsh')
+    {
+        $command = "Add-AppxPackage $wingetBundlePath"
+        $process = Start-Process PowerShell -WorkingDirectory $pwd -NoNewWindow -PassThru -Wait -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"$command`""
+        if ($process.ExitCode -ne 0)
+        {
+            exit $process.ExitCode
+        }
+    }
+    else
+    {
+        Add-AppxPackage $wingetBundlePath
+    }
+
+    Write-Host
+}
+
+function Install-Git
+{
+    if (Get-Command -ErrorAction Ignore -Type Application git.exe)
+    {
+        Write-Host 'git already installed'
+        return
+    }
+
+    if (!(Get-Command -ErrorAction Ignore -Type Application winget.exe))
+    {
+        Write-Host 'winget not installed'
+        return
+    }
+
+    winget.exe install --id Git.Git -e --source winget
+
+    Write-Host
+}
+
+function Install-Choco
+{
+    if (Get-Command -ErrorAction Ignore -Type Application choco.exe)
+    {
+        Write-Host 'choco already installed'
+        return
+    }
+
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+    $Env:Path = [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine) + ";" + [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::User)
+
+    Write-Host
+}
+
+function Install-ChocoPackage
+{
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$package
+    )
+
+    if (!(Get-Command -ErrorAction Ignore -Type Application choco.exe))
+    {
+        Write-Host 'choco not installed'
+        return
+    }
+
+    if (choco list --limit-output --exact $package)
+    {
+        Write-Host "'$package' already installed"
+        return
+    }
+
+    choco install $package -y
+}
+
+$ErrorActionPreference = 'Stop'
+
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+    Write-Warning 'You have to run this script as admin'
+    return
+}
+
+Install-Notepad
+
+Install-Winget
+
+Install-Git
+
+Install-Choco
+
+Install-ChocoPackage make
+Install-ChocoPackage delta
+Install-ChocoPackage gitui
+Install-ChocoPackage ilspy
+Install-ChocoPackage neovim
