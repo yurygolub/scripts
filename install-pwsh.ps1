fbc0aa7fc3980048187993f823989f6772ec886d
Yury Golub <yuragolub21@gmail.com>
2024-03-10 11:58:30 +0300

    Add install-pwsh.ps1 script.

---
+++ install-pwsh.ps1
@@ -0,0 +1,56 @@
+function Get-RedirectedUrl
+{
+    param (
+        [Parameter(Mandatory = $true)]
+        [uri]$url,
+        [uri]$referer
+    )
+
+    $request = [Net.WebRequest]::CreateDefault($url)
+    if ($referer)
+    {
+        $request.Referer = $referer
+    }
+
+    $response = $request.GetResponse()
+
+    if ($response -and $response.ResponseUri.OriginalString -ne $url)
+    {
+        Write-Verbose "Found redirected url '$($response.ResponseUri)'"
+        $result = $response.ResponseUri.OriginalString
+    }
+    else
+    {
+        Write-Warning 'No redirected url was found, returning given url.'
+        $result = $url
+    }
+
+    $response.Dispose()
+
+    return $result
+}
+
+function Install-Pwsh
+{
+    $baseUrl = 'https://github.com/PowerShell/PowerShell'
+    $redirected = Get-RedirectedUrl "$baseUrl/releases/latest"
+
+    $tagName = Split-Path $redirected -Leaf
+    $pwshInstaller = "PowerShell-$($tagName.Substring(1))-win-x64.msi"
+
+    $tempDir = 'temp'
+    $null = New-Item -Path $tempDir -Type Directory -Force
+
+    $pwshInstallerPath = Join-Path $tempDir -ChildPath $pwshInstaller
+    if (!(Test-Path -Path $pwshInstallerPath))
+    {
+        $downloadUrl = "$baseUrl/releases/download/$tagName/$pwshInstaller"
+        Write-Host "Downloading '$pwshInstaller' from '$downloadUrl'"
+        curl.exe -L $downloadUrl -o $pwshInstallerPath
+    }
+
+    $pwshInstallLog = Join-Path $tempDir -ChildPath 'pwsh-install.log'
+    msiexec.exe /package $pwshInstallerPath /log $pwshInstallLog
+}
+
+Install-Pwsh
