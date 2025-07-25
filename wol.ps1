195072c791f2ea93d642f6c8545d935d2878475e
Yury Golub <yuragolub21@gmail.com>
2025-07-25 11:18:53 +0300

    Update wol.

--- wol.ps1
+++ wol.ps1
@@ -1,6 +1,8 @@
-$macAddr = '88-D7-F6-7B-D1-10'
+param (
+    [string] $MacAddr = '88-D7-F6-7B-D1-10'
+)
 
-$macByteArray = $macAddr -split "[:-]" | ForEach-Object { [byte] "0x$_" }
+$macByteArray = $MacAddr -split "[:-]" | ForEach-Object { [byte] "0x$_" }
 
 [byte[]] $magicPacket = (, 0xFF * 6) + ($macByteArray * 16)
 
@@ -8,7 +10,7 @@ $udpClient = [System.Net.Sockets.UdpClient]::new()
 
 try
 {
-    Write-Host "Sending WOL packet to MAC: $macAddr"
+    Write-Host "Broadcasting WOL packet with MAC: $MacAddr"
     $udpClient.Connect([System.Net.IPAddress]::Broadcast, 9)
     $null = $udpClient.Send($magicPacket, $magicPacket.Length)
 }
